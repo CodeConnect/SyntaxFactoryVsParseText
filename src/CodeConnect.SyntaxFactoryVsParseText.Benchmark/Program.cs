5fb6ed9f5444e4073c335da18a0b19b8ced4a0d6
Amadeus Wieczorek <git@amadeusw.com>
2015-08-26 16:26:11 -0700

    One measurement per process

--- src/CodeConnect.SyntaxFactoryVsParseText.Benchmark/Program.cs
+++ src/CodeConnect.SyntaxFactoryVsParseText.Benchmark/Program.cs
@@ -10,65 +10,76 @@ namespace CodeConnect.SyntaxFactoryVsParseText.Benchmark
 {
     class Program
     {
-        const string _outputPath = "benchmark.csv";
-        const int _samples = 1000;
-        const int _runs = 5;
+        const int _parseTextId = 0;
+        const int _syntaxFactoryId = 1;
+        const int _samples = 100;
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Benchmark");
+            Console.WriteLine(args);
+            Console.WriteLine(args.Count());
+            if (args != null && args.Count() == 3)
+            {
+                Console.WriteLine(args[0]);
+                Console.WriteLine(args[1]);
+                Console.WriteLine(args[2]);
+                var desiredCalls = Int32.Parse(args[0]);
+                var desiredComplexity = Boolean.Parse(args[1]);
+                var generatorId = Int32.Parse(args[2]);
+                SingleRun(desiredCalls, desiredComplexity, generatorId);
+            }
+            else
+            {
+                Console.WriteLine("Benchmark");
 
-            ResetOutput();
+                Benchmark(50, false);
+                Benchmark(100, false);
+                Benchmark(500, false);
+                Benchmark(1000, false);
+                Benchmark(5000, false);
 
-            Benchmark(10, false);
-            Benchmark(50, false);
-            Benchmark(100, false);
-            Benchmark(500, false);
-            Benchmark(1000, false);
+                Benchmark(50, true);
+                Benchmark(100, true);
+                Benchmark(500, true);
+                Benchmark(1000, true);
+                Benchmark(5000, true);
 
-            Benchmark(10, true);
-            Benchmark(50, true);
-            Benchmark(100, true);
-            Benchmark(500, true);
-            Benchmark(1000, true);
-
-            Console.ReadLine();
+                Console.ReadLine();
+            }
         }
 
-        private static void ResetOutput()
+        private static void SingleRun(int methodCalls, bool complexMethods, int generatorId)
         {
-            // WriteAllText overwrites existing files
-            File.WriteAllText(_outputPath, $"Generator name, Method count, Complex methods, Run, Elapsed ms\n");
+            CodeGenerator generator = generatorId == _parseTextId ? new ParseTextCodeGenerator() as CodeGenerator : new SyntaxFactoryCodeGenerator() as CodeGenerator;
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            generator.GenerateType(methodCalls, complexMethods);
+            stopwatch.Stop();
+            var outputPath = $"D:\\benchmark {generator.ToString()} {methodCalls} {complexMethods}.csv";
+            File.AppendAllText(outputPath, $"{stopwatch.ElapsedMilliseconds}\n");
         }
 
         static void Benchmark(int methodCalls, bool complexMethods)
         {
-            for (int run = 0; run < _runs; run++)
+            string what = complexMethods ? "methods" : "empty methods";
+            Console.WriteLine($"Generating {methodCalls} {what}, {_samples} times.");
+            ProcessStartInfo startInfo;
+            Process singleRun;
+            for (int sample = 0; sample < _samples; sample++)
             {
-                string what = complexMethods ? "methods" : "empty methods";
-                Console.WriteLine($"Generating {methodCalls} {what}, run {run}.");
-                CodeGenerator generator;
-                var stopwatch = new Stopwatch();
+                // Parse text
+                startInfo = Process.GetCurrentProcess().StartInfo;
+                startInfo.FileName = Process.GetCurrentProcess().MainModule.FileName;
+                startInfo.Arguments = $"{methodCalls} {complexMethods} {_parseTextId}";
+                singleRun = Process.Start(startInfo);
+                singleRun.WaitForExit();
 
-                generator = new ParseTextCodeGenerator();
-                stopwatch.Restart();
-                for (int samples = 0; samples < _samples; samples++)
-                {
-                    generator.GenerateType(methodCalls, complexMethods);
-                }
-                stopwatch.Stop();
-                Console.WriteLine($"{generator.ToString()} finished {_samples} samples in {stopwatch.Elapsed}.");
-                File.AppendAllText(_outputPath, $"{generator.ToString()},{methodCalls},{complexMethods},{run},{stopwatch.ElapsedMilliseconds}\n");
-
-                generator = new SyntaxFactoryCodeGenerator();
-                stopwatch.Restart();
-                for (int samples = 0; samples < 1000; samples++)
-                {
-                    generator.GenerateType(methodCalls, complexMethods);
-                }
-                stopwatch.Stop();
-                Console.WriteLine($"{generator.ToString()} finished {_samples} samples in {stopwatch.Elapsed}.");
-                File.AppendAllText(_outputPath, $"{generator.ToString()},{methodCalls},{complexMethods},{run},{stopwatch.ElapsedMilliseconds}\n");
+                // Syntax factory
+                startInfo = Process.GetCurrentProcess().StartInfo;
+                startInfo.FileName = Process.GetCurrentProcess().MainModule.FileName;
+                startInfo.Arguments = $"{methodCalls} {complexMethods} {_syntaxFactoryId}";
+                singleRun = Process.Start(startInfo);
+                singleRun.WaitForExit();
             }
         }
 
